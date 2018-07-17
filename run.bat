6f72aa240dd81e1a29a6ff8754533d461bbfbb15
Arseniy <afanasenko@smedx.com>
2018-07-17 18:44:06 +0300

    Add path to gdcm binaries

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 set PATH=C:/TOOLS/openslide/bin;%PATH%
+set PATH=C:/TOOLS/gdcm/bin/Release;%PATH%
 set FILE="E:\08.02.2017(6)\244045 Heksa.mrxs"
-cd x64/Release
-mrxconvert.exe %FILE%
+call "x64/Release/mrxconvert.exe" %FILE%
 pause
